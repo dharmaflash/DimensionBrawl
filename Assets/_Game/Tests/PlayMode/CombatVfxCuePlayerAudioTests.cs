2812864d33495f298a6cf584c24bcff986f8ba13
otiple-star <dharmaflash@otiple.co.kr>
2026-06-22 06:52:29 +0900

    feat: add armor break cue audio

--- Assets/_Game/Tests/PlayMode/CombatVfxCuePlayerAudioTests.cs
+++ Assets/_Game/Tests/PlayMode/CombatVfxCuePlayerAudioTests.cs
@@ -81,6 +81,7 @@ namespace DimensionBrawl.Tests
             AssertPromotedCueAudio(profile, CombatVfxCueId.EnemyHit, "light_hit", magicMissilesAudioPath);
             AssertPromotedCueAudio(profile, CombatVfxCueId.EnemyDeath, "death_hit", magicMissilesAudioPath);
             AssertPromotedCueAudio(profile, CombatVfxCueId.EliteShieldSignal, "holy_hit", magicMissilesAudioPath);
+            AssertPromotedCueAudio(profile, CombatVfxCueId.EliteArmorBreakSignal, "light_hit", magicMissilesAudioPath);
             AssertPromotedCueAudio(profile, CombatVfxCueId.EliteSummonSignal, "holy_shoot", magicMissilesAudioPath);
             AssertPromotedCueAudio(profile, CombatVfxCueId.SummonFollowupWindow, "holy_shoot", magicMissilesAudioPath);
             AssertPromotedCueAudio(profile, CombatVfxCueId.SummonBlockOpportunity, "holy_shoot", magicMissilesAudioPath);
