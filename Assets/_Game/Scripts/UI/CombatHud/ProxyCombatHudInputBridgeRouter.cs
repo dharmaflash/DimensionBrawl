d259aeebd736e860283b666f70ff6af5010dd1bc
otiple-star <dharmaflash@otiple.co.kr>
2026-06-26 17:17:56 +0900

    Revert "feat(hud): add combat guide cues for summon qte"
    This reverts commit e9a73887e3f0744be709d6c9f00f87609e0b9aba.

--- Assets/_Game/Scripts/UI/CombatHud/ProxyCombatHudInputBridgeRouter.cs
+++ Assets/_Game/Scripts/UI/CombatHud/ProxyCombatHudInputBridgeRouter.cs
@@ -31,10 +31,7 @@ namespace DimensionBrawl.UI
             new ActionBinding(CombatHudActionId.BasicAttack, ProxyCombatHudInputEvent.BasicAttack()),
             new ActionBinding(CombatHudActionId.Dodge, ProxyCombatHudInputEvent.Dodge()),
             new ActionBinding(CombatHudActionId.Skill1, ProxyCombatHudInputEvent.SignatureSkill()),
-            new ActionBinding(CombatHudActionId.Ultimate, ProxyCombatHudInputEvent.SignatureSkill()),
-            new ActionBinding(CombatHudActionId.SummonSlot1, ProxyCombatHudInputEvent.PartnerSkill()),
-            new ActionBinding(CombatHudActionId.SummonSlot2, ProxyCombatHudInputEvent.SwitchOrQte(1)),
-            new ActionBinding(CombatHudActionId.SummonSlot3, ProxyCombatHudInputEvent.SwitchOrQte(2))
+            new ActionBinding(CombatHudActionId.Ultimate, ProxyCombatHudInputEvent.SignatureSkill())
         };
 
         public ProxyCombatHudTutorialRunner TutorialRunner => tutorialRunner;
